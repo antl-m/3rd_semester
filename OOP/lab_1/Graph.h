169fa29c6cc57416b6a063d058ae28ad00970467
Antl_M <Antl_M@LAPTOP-5N989V8M>
2020-09-29 16:09:48 +0300

    Fully implemented and working classes of Graph; TODO: -interactive mode; -unit tests; -documentation

--- OOP/lab_1/Graph.h
+++ OOP/lab_1/Graph.h
@@ -126,14 +126,10 @@ bool Graph<V, E>::connectivity() const {
     while (!q.empty()) {
         V v = q.front();
         q.pop();
-        std::cerr << "current vertex is " << v << std::endl;
         for (std::size_t i = 0;
              i < _count_adjacent_to_v(v); ++i) {
-            std::cerr << std::endl;
-            std::cerr << std::endl;
             V to = _adjacent_to_v_by_pos(v, i);
             if (!used[to]) {
-                std::cerr << "current adjacent is " << to << std::endl;
                 used[to] = true;
                 q.push(to);
             }
@@ -155,7 +151,7 @@ public:
 
     void push_vertex(const V &v) override;
 
-    std::size_t size() const;
+    std::size_t size() const override;
 
 private:
 
@@ -262,26 +258,16 @@ bool operator<(const Node<V, E> &lhs, const Node<V, E> &rhs) {
 }
 
 template<class V, class E>
+bool operator==(const Node<V, E> &l, const Node<V, E> &r) {
+    return l.vertex == r.vertex;
+}
+
+template<class V, class E>
 class LinkedGraph : public Graph<V, E> {
 public:
-    void push_edge(const E &e, const V &v1, const V &v2) override {
-        if (link_struct.count(v1) == 0 || link_struct.count(v2) == 0) {
-            std::stringstream message;
-            message << "push_edge: no " << (link_struct.count(v1) == 0 ? v1 : v2) << " vertex in graph.";
-            throw GraphExc(message.str());
-        }
-        if (_check_edge(v1, v2)) {
-            std::stringstream message;
-            message << "push_edge: you want to push " << e << " edge at place of " << _edge_of(v1, v2) << " edge.";
-            throw GraphExc(message.str());
-        }
-        link_struct[v1].insert({v2, e});
-        link_struct[v2].insert({v1, e});
-    }
+    void push_edge(const E &e, const V &v1, const V &v2) override;
 
-    void push_vertex(const V &v) override {
-        link_struct.insert(v);
-    }
+    void push_vertex(const V &v) override;
 
 private:
     V _get_by_pos(std::size_t pos) const;
@@ -292,16 +278,96 @@ private:
 
     E _edge_of(const V &v1, const V &v2) const;
 
-    bool _check_edge(const V &v1, const V &v2) const {
-        return link_struct.count(v1) && link_struct.at(v1).find_if(Node{v2, E()},
-                                                                [](const Node<V, E> &l, const Node<V, E> &r) {
-                                                                    return l.vertex == r.vertex;
-                                                                }) != link_struct.at(v1).end();
+    bool _check_edge(const V &v1, const V &v2) const;
 
-    }
+    std::size_t size() const override;
 
     std::map<V, std::set<Node<V, E>>> link_struct;
 };
+
+template<class V, class E>
+void LinkedGraph<V, E>::push_edge(const E &e, const V &v1, const V &v2) {
+    if (link_struct.count(v1) == 0 || link_struct.count(v2) == 0) {
+        std::stringstream message;
+        message << "push_edge: no " << (link_struct.count(v1) == 0 ? v1 : v2) << " vertex in graph.";
+        throw GraphExc(message.str());
+    }
+    if (_check_edge(v1, v2)) {
+        std::stringstream message;
+        message << "push_edge: you want to push " << e << " edge at place of " << _edge_of(v1, v2) << " edge.";
+        throw GraphExc(message.str());
+    }
+    link_struct[v1].insert({v2, e});
+    link_struct[v2].insert({v1, e});
+}
+
+template<class V, class E>
+void LinkedGraph<V, E>::push_vertex(const V &v) {
+    link_struct.insert({v, {}});
+}
+
+template<class V, class E>
+bool LinkedGraph<V, E>::_check_edge(const V &v1, const V &v2) const {
+    return link_struct.count(v1) && link_struct.at(v1).find(Node<V, E>{v2, E()}) != link_struct.at(v1).end();
+
+}
+
+template<class V, class E>
+V LinkedGraph<V, E>::_get_by_pos(std::size_t pos) const {
+    std::size_t cur = 0;
+    for (const auto &[vert, node_set]: link_struct) {
+        if (cur == pos)
+            return vert;
+        ++cur;
+    }
+    throw GraphExc("_get_by_pos: pos " + std::to_string(pos) + " out of range");
+}
+
+template<class V, class E>
+std::size_t LinkedGraph<V, E>::_count_adjacent_to_v(const V &v) const {
+    if (link_struct.count(v)) {
+        return link_struct.at(v).size();
+    }
+    std::stringstream message;
+    message << "_count_adjacent_to_v: no vertex " << v << " in graph";
+    throw GraphExc(message.str());
+}
+
+template<class V, class E>
+V LinkedGraph<V, E>::_adjacent_to_v_by_pos(const V &v, std::size_t pos) const {
+    if (link_struct.count(v)) {
+        std::size_t cur = 0;
+        for (const auto&[vert, edg]: link_struct.at(v)) {
+            if (cur == pos)
+                return vert;
+            ++cur;
+        }
+    }
+    std::stringstream message;
+    message << "_adjacent_to_v_by_pos: no vertex " << v << " in graph";
+    throw GraphExc(message.str());
+}
+
+template<class V, class E>
+E LinkedGraph<V, E>::_edge_of(const V &v1, const V &v2) const {
+    if (link_struct.count(v1) == 0 || link_struct.count(v2) == 0) {
+        std::stringstream message;
+        message << "_edge_of: no " << (link_struct.count(v1) == 0 ? v1 : v2) << " vertex in graph.";
+        throw GraphExc(message.str());
+    }
+    if (_check_edge(v1, v2)) {
+        auto node_it = link_struct.at(v1).find(Node<V, E>{v2, E()});
+        return node_it->edge;
+    }
+    std::stringstream message;
+    message << "_edge_of: no edge between " << v1 << " and " << v2 << ".";
+    throw GraphExc(message.str());
+}
+
+template<class V, class E>
+std::size_t LinkedGraph<V, E>::size() const {
+    return link_struct.size();
+}
 
 
 #endif //GRAPH_H
--- OOP/lab_1/main.cpp
+++ OOP/lab_1/main.cpp
@@ -23,30 +23,26 @@ bool operator<(const a &l, const a &r){
 }
 
 int main() {
-//    MatrixGraph<int, int> g;
-//    g.push_vertex(1);
-//    g.push_vertex(2);
-//    g.push_vertex(3);
-//    g.push_vertex(4);
-//    g.push_vertex(5);
-//    g.push_vertex(6);
-//    g.push_edge(5, 1, 4);
-//    g.push_edge(1, 3, 4);
-//    g.push_edge(2, 3, 5);
-//    g.push_edge(1, 4, 6);
-//    std::cout << std::boolalpha << g.connectivity() << std::endl;
-//    g.push_edge(20, 1, 2);
-//    g.push_edge(1, 6, 2);
-//    std::cout << g.connectivity() << std::endl;
-//    std::cout << g.length(1, 2)[0] << std::endl;
-//    std::cout << g.length(1, 5)[0] << std::endl;
-//    g.push_edge(1, 4, 5);
-//    std::cout << g.length(1, 5)[0] << std::endl;
-//    std::cout << g.length(4, 2)[0] << std::endl;
-//    g.push_edge(1,6, 2);
-//    std::cout << g.length(1, 2)[0] << std::endl;
-//
-//    MatrixGraph<a, a> asd;
+    LinkedGraph<double, int> g;
+    g.push_vertex(1);
+    g.push_vertex(2);
+    g.push_vertex(3);
+    g.push_vertex(4);
+    g.push_vertex(5);
+    g.push_vertex(6);
+    g.push_edge(5, 1, 4);
+    g.push_edge(1, 3, 4);
+    g.push_edge(2, 3, 5);
+    g.push_edge(1, 4, 6);
+    std::cout << std::boolalpha << g.connectivity() << std::endl;
+    g.push_edge(20, 1, 2);
+    g.push_edge(1, 6, 2);
+    std::cout << g.connectivity() << std::endl;
+    std::cout << g.length(1, 2)[0] << std::endl;
+    std::cout << g.length(1, 5)[0] << std::endl;
+    g.push_edge(1, 4, 5);
+    std::cout << g.length(1, 5)[0] << std::endl;
+    std::cout << g.length(4, 2)[0] << std::endl;
 
 
     return 0;
