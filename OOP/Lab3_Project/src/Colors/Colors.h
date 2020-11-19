74e8b6341e3a34b1d577ed6cdd4839670d9a3fe8
antl-m <tolikmalibroda@gmail.com>
2020-11-19 23:37:07 +0200

    Lab3_Project: color theme changed

--- OOP/Lab3_Project/src/Colors/Colors.h
+++ OOP/Lab3_Project/src/Colors/Colors.h
@@ -2,7 +2,7 @@
 #define SFMLGAME_SRC_COLORS_COLORS_H_
 
 #include "SFML/Graphics.hpp"
-
+/***
 namespace MyColors {
   const inline sf::Color kPlayerColor(177, 235, 0);
   const inline sf::Color kOpponentColor(255, 67, 46);
@@ -10,5 +10,23 @@ namespace MyColors {
   const inline sf::Color kTileOutlineColor(12, 126, 188);
   const inline sf::Color kWallColor(255, 172, 0);
 }
-
+***/
+/***
+namespace MyColors {
+  const inline sf::Color kPlayerColor(3, 167, 98);
+  const inline sf::Color kOpponentColor(237, 27, 36);
+  const inline sf::Color kTileFillColor(96, 141, 200);
+  const inline sf::Color kTileOutlineColor(3, 103, 179);
+  const inline sf::Color kWallColor(244, 134, 37);
+}
+***/
+namespace MyColors {
+//const inline sf::Color kPlayerColor(3, 167, 98);
+//const inline sf::Color kOpponentColor(237, 27, 36);
+const inline sf::Color kPlayerColor(104, 195, 152);
+const inline sf::Color kOpponentColor(244, 123, 112);
+const inline sf::Color kTileFillColor(178, 199, 230);
+const inline sf::Color kTileOutlineColor(96, 141, 200);
+const inline sf::Color kWallColor(244, 171, 118);
+}
 #endif //SFMLGAME_SRC_COLORS_COLORS_H_
